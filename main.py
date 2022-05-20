d06eaa813cb6ac5d51ba9c7c1860db818a2d7190
Sebastian Henao <sebastianhenaoloaiza@gmail.com>
2022-05-19 19:11:27 -0500

    Solucion Inicial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from openpyxl import load_workbook
 import json
-import os
 import shutil
 
 def move_invoices(book_path, sheet_name, json_path, invoices_path):
@@ -16,13 +15,18 @@ def move_invoices(book_path, sheet_name, json_path, invoices_path):
   with open(json_path) as data:
     data = json.load(data)
 
-  # List Files
-  files = os.listdir(invoices_path)
-
   for row in sheet.iter_rows(min_row=2, values_only=True):
-    print(row)
+    try:
+      if row[1] == 'Excluido':
+        shutil.move(invoices_path+'FE'+str(row[0])+'.pdf', data['Excluido'])
+      elif row[1] == 'Exentas':
+        shutil.move(invoices_path+'FE'+str(row[0])+'.pdf', data['Exentas'])
+      elif row[1] == 'Reintegros':
+        shutil.move(invoices_path+'FE'+str(row[0])+'.pdf', data['Reintegros'])
+    except:
+      print('Error')
 
 
 
-move_invoices('./book/test.xlsx', None, './paths.json', './invoices')
+move_invoices('./book/detalle.xlsx', None, './paths.json', 'C:/Users/auxiliar.tics/Desktop/Facturas/')
 
